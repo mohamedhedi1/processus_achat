b32a1aa39cd694352957be0d0a1f54db73c7bdd4
unknown <th7kingdom@gmail.com>
2022-03-06 20:26:56 +0100

    first commit

---
+++ processus_backend/src/main/java/com/example/processus_backend/ProcessusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.processus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessusBackendApplication.class, args);
+	}
+
+}
---
+++ processus_backend/src/test/java/com/example/processus_backend/ProcessusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.processus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
